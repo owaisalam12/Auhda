8dc853fa737196f061c768bd8f91f87aba5df38c
owaisalam12 <owaisalamkhi@gmail.com>
2019-07-22 12:34:37 +0500

    signup signin before order

--- app/src/main/java/com/core2plus/auhda/Fragment/BlogDetailsFragment.java
+++ app/src/main/java/com/core2plus/auhda/Fragment/BlogDetailsFragment.java
@@ -1,20 +1,16 @@
 package com.core2plus.auhda.Fragment;
 
 
-import android.graphics.Movie;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.core2plus.auhda.API.Responses.auhdaResponse;
+import androidx.fragment.app.Fragment;
+
 import com.core2plus.auhda.R;
 
 /**
